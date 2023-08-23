1e266b70d87e14581198997f0fafe7af53fb24fa
Eduardo Kiriakos <eduardokiriakos@gmail.com>
2023-08-23 15:56:02 +0200

    feat: create sales module and dto

---
+++ src/api/sales/sales.dto.ts
@@ -0,0 +1,72 @@
+import {
+  IsNotEmpty,
+  MaxLength,
+  IsInt,
+  IsOptional,
+  IsArray,
+  ValidateNested,
+} from 'class-validator';
+import { CreateOrderDto, OrderWithId } from '../orders/orders.dto';
+import { Type } from 'class-transformer';
+import { CreatePaymentDto, PaymentWithId } from '../payments/payments.dto';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+export class CreateSaleDto {
+  @ApiProperty({
+    type: CreateOrderDto,
+  })
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => CreateOrderDto)
+  orders!: CreateOrderDto[];
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @MaxLength(255)
+  @IsNotEmpty()
+  description?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsInt()
+  clientId?: number;
+
+  @ApiPropertyOptional({
+    type: CreatePaymentDto,
+  })
+  @IsOptional()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => CreatePaymentDto)
+  payments?: CreatePaymentDto[];
+}
+
+export class UpdateSaleDto {
+  @ApiPropertyOptional()
+  @IsOptional()
+  @MaxLength(255)
+  @IsNotEmpty()
+  description?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsInt()
+  clientId?: number;
+}
+
+export class SaleWithId {
+  @ApiProperty()
+  id!: number;
+
+  @ApiPropertyOptional()
+  description?: string;
+
+  @ApiPropertyOptional()
+  clientId?: number;
+
+  @ApiPropertyOptional()
+  payments?: PaymentWithId[];
+
+  @ApiPropertyOptional()
+  orders?: OrderWithId[];
+}
