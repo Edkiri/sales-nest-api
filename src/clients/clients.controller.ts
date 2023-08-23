ffe621da93185e28811204bab361da07461454b8
Eduardo Kiriakos <eduardokiriakos@gmail.com>
2023-08-23 10:39:44 +0200

    feat: add prisma not found exception

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  ParseIntPipe,
+  Put,
+  Param,
+  Delete,
+  HttpCode,
+} from '@nestjs/common';
 import { ClientsService } from './clients.service';
-import { CreateClientDto } from './clients.dto';
+import { CreateClientDto, UpdateClientDto } from './clients.dto';
 
 @Controller('clients')
 export class ClientsController {
@@ -8,6 +18,29 @@ export class ClientsController {
 
   @Post()
   createClient(@Body() data: CreateClientDto) {
-    return this.clientsService.createClient(data);
+    return this.clientsService.createOne(data);
+  }
+
+  @Get()
+  findClients() {
+    return this.clientsService.findAll();
+  }
+
+  @Get(':clientId')
+  findClient(@Param('clientId', ParseIntPipe) clientId: number) {
+    return this.clientsService.findOne(clientId);
+  }
+  @Put(':clientId')
+  updateClient(
+    @Param('clientId', ParseIntPipe) clientId: number,
+    @Body() data: UpdateClientDto,
+  ) {
+    return this.clientsService.updateOne(clientId, data);
+  }
+
+  @Delete(':clientId')
+  @HttpCode(204)
+  deleteClient(@Param('clientId', ParseIntPipe) clientId: number) {
+    return this.clientsService.deleteOne(clientId);
   }
 }
--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -1,12 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { Client, Prisma } from '@prisma/client';
 import { PrismaService } from 'src/database/prisma.service';
+import { UpdateClientDto } from './clients.dto';
 
 @Injectable()
 export class ClientsService {
   constructor(private prisma: PrismaService) {}
 
-  async createClient(data: Prisma.ClientCreateInput): Promise<Client> {
+  async findOne(clientId: number): Promise<Client> {
+    return this.prisma.client.findUniqueOrThrow({ where: { id: clientId } });
+  }
+
+  async createOne(data: Prisma.ClientCreateInput): Promise<Client> {
     return await this.prisma.client.create({ data });
+  }
+
+  async findAll(): Promise<Client[]> {
+    return await this.prisma.client.findMany();
+  }
+
+  async updateOne(clientId: number, data: UpdateClientDto): Promise<Client> {
+    return await this.prisma.client.update({
+      where: { id: clientId },
+      data,
+    });
+  }
+
+  async deleteOne(clientId: number): Promise<void> {
+    await this.prisma.client.delete({ where: { id: clientId } });
+    return;
   }
 }
